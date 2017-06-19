d7b3523cecc3dc90e99d38386adfc3a84f98718f
iwebnal <iwebnal@gmail.com>
2017-06-19 15:08:41 +0300

    a

--- layout/include/content.php
+++ layout/include/content.php
@@ -1,3 +1,12 @@
-<div class="container">
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio iure earum quis voluptas, corporis a repellat reprehenderit ex placeat optio! Fugiat odio at obcaecati ducimus alias perspiciatis harum aliquam, sunt?
+<div class="contain">
+	<div class="topbox">
+		<span class="topbox-title">Поможем выбрать и качественно реализовать умное ИТ-решение для вашего бизнеса</span>
+		<div class="topbox-wrapp">
+			<div class="topbox-item">
+				<span class="topbox-item-title">бизнес–решение</span>
+				<p class="topbox-item-title">здесь написать пару слов для этого блока, чтобы было ясно, что будем вместе подбирать</p>
+				<a href="#" class="topbox-item-link">Подробнее</a>
+			</div>
+		</div>
+	</div>
 </div>
