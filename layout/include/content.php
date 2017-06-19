7b1f177519b99c8ed6d885b091e5d9104e664ec1
hsrazna <anzar.terek@gmail.com>
2017-06-19 14:57:49 +0300

    sdf

--- layout/include/content.php
+++ layout/include/content.php
@@ -1,1 +1,3 @@
-<div class="az-container"></div>
+<div class="container">
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio iure earum quis voluptas, corporis a repellat reprehenderit ex placeat optio! Fugiat odio at obcaecati ducimus alias perspiciatis harum aliquam, sunt?
+</div>
