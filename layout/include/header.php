1f174997cbf4b93cb6b9ebdac1e315ff568c783b
hsrazna <anzar.terek@gmail.com>
2017-06-19 17:08:37 +0300

    header and sidebar

--- layout/include/header.php
+++ layout/include/header.php
@@ -3,6 +3,10 @@
 		<img src="img/header-logo.png" alt="VPS Hoster">
 	</div>
 	<div class="header-right">
-		<a href="#" class="header-phone">+7 499 112-40-45</a>
+		<a href="#" class="header-phone header-marginr">+7 499 112-40-45</a>
+		<a href="#" class="header-envelope header-marginr2">info@virtualdc.ru</a>
+		<a href="#" class="header-lk header-marginr3">Личный кабинет</a>
+		<a href="#" class="header-reg  header-marginr4">Регистрация</a>
+		<a href="#" class="header-cart">Корзина</a>
 	</div>
 </div>
--- layout/include/sitebar.php
+++ layout/include/sitebar.php
@@ -1,1 +1,10 @@
-<div class="sidebar"></div>
+<nav class="sidebar">
+	<ul class="sidebar-menu">
+		<li><a href="#" class="sidebar-menu__link1">Главная</a></li>
+		<li><a href="#" class="sidebar-menu__link2">Облачный хостинг</a></li>
+		<li><a href="#" class="sidebar-menu__link3">Выделенный сервер</a></li>
+		<li><a href="#" class="sidebar-menu__link4">Частное облако</a></li>
+		<li><a href="#" class="sidebar-menu__link5">Решения для бизнеса</a></li>
+		<li><a href="#" class="sidebar-menu__link6">Аутсорсинг</a></li>
+	</ul>
+</nav>
