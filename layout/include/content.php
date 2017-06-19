bda54bf129bce715e8bfee9975d3f661fe51403a
ahatuhov <ahatuhov@gmail.com>
2017-06-19 14:14:15 +0300

    a

--- layout/include/content.php
+++ layout/include/content.php
@@ -1,1 +1,5 @@
-<div class="az-container"></div>
+<div class="az-container">
+	
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsum repellat repellendus totam illo doloremque aliquam fugit dicta maiores unde sit animi, provident harum voluptatem corporis numquam dolorem facere enim ducimus!
+
+</div>
