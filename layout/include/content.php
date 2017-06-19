09ca0e31550bf3dc7bd98f34f68c4035c21aa9f5
iwebnal <iwebnal@gmail.com>
2017-06-19 17:06:16 +0300

    a

--- layout/include/content.php
+++ layout/include/content.php
@@ -1,11 +1,39 @@
 <div class="contain">
 	<div class="topbox">
-		<span class="topbox-title">Поможем выбрать и качественно реализовать умное ИТ-решение для вашего бизнеса</span>
+		<div class="topbox-title-wrapp">
+			<span class="topbox-title">Поможем выбрать и качественно реализовать умное ИТ-решение для вашего бизнеса</span>
+		</div>
 		<div class="topbox-wrapp">
 			<div class="topbox-item">
 				<span class="topbox-item-title">бизнес–решение</span>
-				<p class="topbox-item-title">здесь написать пару слов для этого блока, чтобы было ясно, что будем вместе подбирать</p>
+				<p class="topbox-item-text">здесь написать пару слов для этого блока, чтобы было ясно, что будем вместе подбирать</p>
 				<a href="#" class="topbox-item-link">Подробнее</a>
+			</div>
+			<div class="topbox-item">
+				<span class="topbox-item-title">IT–решение</span>
+				<p class="topbox-item-text">здесь написать пару слов для этого блока, чтобы было ясно, что будем вместе подбирать</p>
+				<a href="#" class="topbox-item-link">Подробнее</a>
+			</div>
+			<div class="topbox-item">
+				<span class="topbox-item-title">индивидуальное решение</span>
+				<p class="topbox-item-text">здесь написать пару слов для этого блока, чтобы было ясно, что будем вместе подбирать</p>
+				<a href="#" class="topbox-item-link">Подробнее</a>
+			</div>
+		</div>
+	</div>
+	<div class="box1">
+		<div class="box1-left">
+			<div class="box1-left-wrapp">
+				<span class="box1-left-title">Виртуальный сервер</span>
+				<span class="box1-left-subtitle">от 1000 руб.</span>
+				<p class="box1-left-text">Здесь написать копирайт для данного блока. Он может состоять из трёх-четрыёх строчек, желательно – три строки.</p>
+			</div>
+		</div>
+		<div class="box1-right">
+			<div class="box1-right-wrapp">
+				<span class="box1-left-title box1-right-title">Решения для интернет-магазинов</span>
+				<span class="box1-left-subtitle">от 1000 руб.</span>
+				<p class="box1-left-text box1-right-text">Здесь написать копирайт для блока. Он может состоять из 1-2 строк.</p>
 			</div>
 		</div>
 	</div>
