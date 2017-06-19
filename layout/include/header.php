1b1b234b8bf33fadffc966dd2d050727b9a54735
hsrazna <anzar.terek@gmail.com>
2017-06-19 14:56:25 +0300

    sdf

--- layout/include/header.php
+++ layout/include/header.php
@@ -1,1 +1,8 @@
-<div class="header"></div>
+<div class="header">
+	<div class="header-logo">
+		<img src="img/header-logo.png" alt="VPS Hoster">
+	</div>
+	<div class="header-right">
+		<a href="#" class="header-phone">+7 499 112-40-45</a>
+	</div>
+</div>
