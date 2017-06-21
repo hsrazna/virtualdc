ba3e89f7b046b2244a17d1c57bb4425c19c4448a
hsrazna <anzar.terek@gmail.com>
2017-06-21 11:34:59 +0300

    our partners

---
+++ layout/include/content-bottom.php
@@ -0,0 +1,22 @@
+<div class="contain">
+	<div class="partner">
+		<div class="partner-item partner-item_title">
+			<h2 class="partner-title">наши клиенты <br>и партнёры</h2>
+		</div>
+		<div class="partner-item">
+			<img src="img/partner1.png" alt="Сбербанк Технологии">
+		</div>
+		<div class="partner-item">
+			<img src="img/partner2.png" alt="Ericsson">
+		</div>
+		<div class="partner-item">
+			<img src="img/partner3.png" alt="Банк Русский Стандарт">
+		</div>
+		<div class="partner-item">
+			<img src="img/partner4.png" alt="Total">
+		</div>
+		<div class="partner-item">
+			<img src="img/partner5.png" alt="Liebherr">
+		</div>
+	</div>
+</div>
