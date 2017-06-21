ade9c80ab7f86a32533ca4a9257693ff04a5f248
hsrazna <anzar.terek@gmail.com>
2017-06-21 13:30:35 +0300

    news

--- layout/include/content-bottom.php
+++ layout/include/content-bottom.php
@@ -1,4 +1,44 @@
 <div class="contain">
+
+	<div class="news">
+		<div class="news-item news-item_title">
+			<h2 class="news-title"><a href="#">новости<br> компании</a></h2>
+		</div>		
+		<div class="news-item news-item_rotate">
+			<a href="#">
+				<div class="news-item-img">
+					<img src="img/news.jpg" alt="Все больше российских компаний предпочитают гендиректоров постарше">
+				</div>
+				<div class="news-item-cont">
+					<h3 class="news-name">Все больше российских компаний предпочитают гендиректоров постарше</h3>
+					<time class="news-time">4 апреля 2017</time>
+				</div>
+			</a>
+		</div>		
+		<div class="news-item news-item_rotate">
+			<a href="#">
+				<div class="news-item-img">
+					<img src="img/news.jpg" alt="Все больше российских компаний предпочитают гендиректоров постарше">
+				</div>
+				<div class="news-item-cont">
+					<h3 class="news-name">Все больше российских компаний предпочитают гендиректоров постарше</h3>
+					<time class="news-time">4 апреля 2017</time>
+				</div>
+			</a>
+		</div>
+		<div class="news-item news-item_rotate">
+			<a href="#">
+				<div class="news-item-img">
+					<img src="img/news.jpg" alt="Все больше российских компаний предпочитают гендиректоров постарше">
+				</div>
+				<div class="news-item-cont">
+					<h3 class="news-name">Все больше российских компаний предпочитают гендиректоров постарше</h3>
+					<time class="news-time">4 апреля 2017</time>
+				</div>
+			</a>
+		</div>	
+	</div>
+
 	<div class="partner">
 		<div class="partner-item partner-item_title">
 			<h2 class="partner-title">наши клиенты <br>и партнёры</h2>
@@ -19,4 +59,5 @@
 			<img src="img/partner5.png" alt="Liebherr">
 		</div>
 	</div>
+
 </div>
