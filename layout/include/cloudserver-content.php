5f33c2ba18cd7b542ca58a8d65c6a12b1583a7ce
hsrazna <anzar.terek@gmail.com>
2017-06-30 16:50:32 +0300

    price

---
+++ layout/include/cloudserver-content.php
@@ -0,0 +1,131 @@
+<div class="contain">
+	<div class="cloudserver-breadcrumbs">
+		<ul class="cloudbreadcrumbs-list">
+			<li><a href="#">Главная</a></li>
+			<li><span></span></li>
+			<li><a href="#">Облачный хостинг</a></li>
+			<li><span></span></li>
+			<li><a class="cloudbreadcrumbs-active">Облачный сервер</a></li>
+		</ul>
+	</div>
+	<div class="cloudserver-top">
+		<span class="cloudserver-toptitle">Облачный сервер</span>
+		<div class="cloudserver-topwrapp">
+			<div class="cloudserver-leftbox">
+				<img src="img/cloudserver-img.png" class="responsive-img" alt="img">
+			</div>
+			<div class="cloudserver-rightbox">
+				<ul class="cloudserver-rightbox-list">
+					<li><span>Сайт рыбатекст поможет дизайнеру</span></li>
+					<li><span>Верстальщику, вебмастеру сгенерировать несколько абзацев</span></li>
+					<li><span>Более менее осмысленного текста рыбы на русском языке</span></li>
+					<li><span>А начинающему оратору отточить навык публичных выступлений</span></li>
+					<li><span>В домашних условиях. При создании генератора мы использовали</span></li>
+					<li><span>Небезизвестный универсальный код речей</span></li>
+					<li><span>Текст генерируется абзацами случайным образом от двух до десяти</span></li>
+					<li><span>Предложений в абзаце, что позволяет сделать текст более</span></li>
+					<li><span>Привлекательным и живым для визуально-слухового восприятия</span></li>
+				</ul>
+			</div>
+		</div>
+		<div class="cloudserver-topwrapp">
+			<div class="cloudserver-leftbox">
+				 <ul class="cloudserver-bottomlist">
+				 	<li><a href="#">Описание</a></li>
+				 	<li><a href="#">цены</a></li>
+				 	<li><a href="#">панель управления</a></li>
+				 	<li><a href="#">шаблоны</a></li>
+				 </ul>
+			</div>
+			<div class="cloudserver-rightbox">
+				<div class="cloudserver-rightlinks">
+					<a href="#" class="cloudserver-rightlink">Заказать</a>
+					<a href="#" class="cloudserver-rightlink1">Попробовать бесплатно</a>
+				</div>
+			</div>
+		</div>	
+	</div>
+	<div class="cloudserver-configbox">
+		<span class="cloudserver-configtitle">Стоимость аренды виртуального сервера от <span>1000 руб./месяц</span></span>
+		<a href="#" class="cloudserver-configlink">Конфигуратор цен</a>
+	</div>
+	<div class="cloudserver-textbox">
+		<div class="cloudserver-textbox1">
+			<div class="cloudserver-textbox1-wrapp">
+				<span class="cloudserver-textbox1-span">Виртуальный сервер на базе Enterprise решений</span>
+			</div>
+		</div>
+		<div class="cloudserver-textbox2">
+			<p class="cloudserver-textbox2-span">Значимость этих проблем настолько очевидна, что постоянный количественный рост и сфера нашей активности позволяет выполнять важные задания по разработке существенных финансовых и административных условий. Не следует, однако забывать, что начало повседневной работы по формированию позиции играет важную роль в формировании систем массового участия. Задача организации, в особенности же сложившаяся структура организации требуют от нас анализа направлений прогрессивного развития.</p>
+			<p class="cloudserver-textbox2-span1 ">Равным образом консультация с широким активом позволяет выполнять важные задания по разработке системы обучения кадров, соответствует насущным потребностям.</p>
+		</div>
+	</div>
+	<div class="cloudserver-textbox">
+		<div class="cloudserver-textbox1">
+			<div class="cloudserver-textbox1-wrapp">
+				<span class="cloudserver-textbox1-span">Основные преимущества облачных серверов</span>
+			</div>
+		</div>
+		<div class="cloudserver-textbox2">
+			<div class="cloudserver-textbox2-item">
+				<div class="cloudserver-imgbox">
+					<img src="img/cloudserver1.png" alt="img">
+				</div>
+				<div class="cloudserver-item-wrtext">
+					<span class="cloudserver-item-title">высокий уровень надежности</span>
+					<p class="cloudserver-item-text">не ниже 99,95%</p>
+				</div>
+			</div>
+			<div class="cloudserver-textbox2-item">
+				<div class="cloudserver-imgbox">
+					<img src="img/cloudserver2.png" alt="img">
+				</div>
+				<div class="cloudserver-item-wrtext">
+					<span class="cloudserver-item-title">масштабируемость ресурсов</span>
+					<p class="cloudserver-item-text">в том числе в автоматическом режиме (autoscale)</p>
+				</div>
+			</div>
+			<div class="cloudserver-textbox2-item">
+				<div class="cloudserver-imgbox">
+					<img src="img/cloudserver3.png" alt="img">
+				</div>
+				<div class="cloudserver-item-wrtext">
+					<span class="cloudserver-item-title">возможность установки любой оС</span>
+					<p class="cloudserver-item-text">в том числе MS Windows</p>
+				</div>
+			</div>
+			<div class="cloudserver-textbox2-item">
+				<div class="cloudserver-imgbox">
+					<img src="img/cloudserver4.png" alt="img">
+				</div>
+				<div class="cloudserver-item-wrtext">
+					<span class="cloudserver-item-title">бесплатная техническая поддержка</span>
+					<p class="cloudserver-item-text">Мы предоставляем бесплатную техническую поддержку на уровне консультаций. Технические работы на облачных серверах осуществляются только по согласованию и на платной основе.</p>
+				</div>
+			</div>
+			<div class="cloudserver-textbox2-item">
+				<div class="cloudserver-imgbox">
+					<img src="img/cloudserver5.png" alt="img">
+				</div>
+				<div class="cloudserver-item-wrtext">
+					<span class="cloudserver-item-title">бесплатное автоматическое резервное копирование</span>
+					<p class="cloudserver-item-text">Мы делаем резервную копию вашего облачного сервера бесплатно, 1 раз в неделю. Ежедневное резервное копирование доступно по запросу и за отдельную плату.</p>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="formbox">
+		<div class="formbox-left">
+			<span class="formbox-title">Вам нужно <span>индивидуальное</span> решение? Мы поможем.</span>
+		</div>
+		<div class="formbox-right">
+			<form class="helping-form">
+				<input type="text" placeholder="Ваш телефон">
+				<input type="submit" value="">
+				<div class="formbox-textwrapp">
+					<p class="formbox-text">Наш менеджер перезвонит Вам в течение рабочего дня и поможет решить вашу задачу</p>
+				</div>
+			</form>
+		</div>
+	</div>
+</div>
