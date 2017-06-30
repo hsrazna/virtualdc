8944a0a7af4582e71b1207784796bc61e7c5d6fc
hsrazna <anzar.terek@gmail.com>
2017-06-30 10:56:57 +0300

    price start

---
+++ layout/include/price.php
@@ -0,0 +1,128 @@
+<div class="contain">
+	<div class="price">
+		<b class="price-notice">Обратите внимание, что постоянный количественный рост и сфера нашей активности позволяет выполнять важные задания по разработке существенных финансовых и административных условий.</b>
+		<div class="price-head">
+			<h2 class="price-title">Технический калькулятор</h2>
+			<a href="#" class="price-title-link">Бизнес-калькулятор</a>
+			<a href="#" class="price-title-link">Помогите выбрать</a>
+		</div>
+		<div class="price-cont">
+			<div class="price-cont-left">
+				<h3 class="price-title2">Операционная система</h3>
+				<div class="price-block">
+					<label class="price-radio price-radio_m"><input type="radio" name="os" checked><span></span>Linux</label>
+					<label class="price-radio"><input type="radio" name="os"><span></span>Windows</label>
+				</div>
+				<h3 class="price-title2">Количество процессоров</h3>
+				<div class="price-block">
+					<div class="price-block-left">
+						<div class="price-slider-wrap">
+							<div class="price-slider"></div>
+							<input type="text" name="min-val" value="0">
+							<input type="text" name="max-val" value="24">
+						</div>
+						<div class="price-slider-marks">
+							<div class="price-mark">1</div>
+							<div class="price-mark">6</div>
+							<div class="price-mark">12</div>
+							<div class="price-mark">18</div>
+							<div class="price-mark">24</div>
+						</div>
+					</div>
+					<div class="price-block-right">
+						<div class="price-range-input_top">
+							<input type="text" name="" class="price-range-input" value="0">
+							<span class="price-range-name">штук</span>
+						</div>
+					</div>
+				</div>
+				<h3 class="price-title2">Оперативная память</h3>
+				<div class="price-block">
+					<div class="price-block-left">
+						<div class="price-slider-wrap">
+							<div class="price-slider"></div>
+							<input type="text" name="min-val" value="0">
+							<input type="text" name="max-val" value="96">
+						</div>
+						<div class="price-slider-marks">
+							<div class="price-mark">1</div>
+							<div class="price-mark">24</div>
+							<div class="price-mark">48</div>
+							<div class="price-mark">72</div>
+							<div class="price-mark">96</div>
+						</div>
+					</div>
+					<div class="price-block-right">
+						<div class="price-range-input_top">
+							<input type="text" name="" class="price-range-input" value="72">
+							<span class="price-range-name">гб.</span>
+						</div>
+					</div>
+				</div>
+				<h3 class="price-title2">Тип диска</h3>
+				<div class="price-block">
+					<label class="price-radio price-radio_m"><input type="radio" name="hd"><span></span>SAS</label>
+					<label class="price-radio"><input type="radio" name="hd" checked><span></span>SSD</label>
+				</div>
+				<h3 class="price-title2">Объём диска</h3>
+				<div class="price-block">
+					<div class="price-block-left">
+						<div class="price-slider-wrap">
+							<div class="price-slider-log"></div>
+							<input type="text" name="min-val" value="20">
+							<input type="text" name="max-val" value="180">
+						</div>
+						<div class="price-slider-marks">
+							<div class="price-mark">20</div>
+							<div class="price-mark">40</div>
+							<div class="price-mark">60</div>
+							<div class="price-mark">80</div>
+							<div class="price-mark">100</div>
+							<div class="price-mark">200</div>
+							<div class="price-mark">600</div>
+							<div class="price-mark">1000</div>
+							<div class="price-mark">2000</div>
+						</div>
+					</div>
+					<div class="price-block-right">
+						<div class="price-range-input_top">
+							<input type="text" name="" class="price-range-input-log" value="160">
+							<span class="price-range-name">гб.</span>
+						</div>
+					</div>
+				</div>
+				<h3 class="price-title2">Количество IP</h3>
+				<div class="price-block">
+					<div class="price-block-left">
+						<div class="price-slider-wrap">
+							<div class="price-slider-log2"></div>
+							<input type="text" name="min-val" value="0">
+							<input type="text" name="max-val" value="90">
+						</div>
+						<div class="price-slider-marks">
+							<div class="price-mark">1</div>
+							<div class="price-mark">10</div>
+							<div class="price-mark">20</div>
+							<div class="price-mark">30</div>
+							<div class="price-mark">40</div>
+							<div class="price-mark">50</div>
+							<div class="price-mark">100</div>
+							<div class="price-mark">150</div>
+							<div class="price-mark">200</div>
+							<div class="price-mark">254</div>
+						</div>
+					</div>
+					<div class="price-block-right">
+						<div class="price-range-input_top">
+							<input type="text" name="" class="price-range-input-log2" value="0">
+							<span class="price-range-name">штук</span>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="price-cont-right">
+				
+			</div>
+		</div>
+	</div>
+</div>
--- layout/js/common2.js
+++ layout/js/common2.js
@@ -5,4 +5,166 @@ $(function() {
 		return false;
 	});
 
+	$(".price-slider").each(function(){
+		var pmin = parseInt($(this).siblings("input[name=min-val]").val());
+		var pmax = parseInt($(this).siblings("input[name=max-val]").val());
+		var pval = parseInt($(this).parents(".price-block").find(".price-range-input").val());
+		$(this).parents(".price-block").find(".price-range-input").val(f1(pval));//)
+		$(this).slider({
+			min: pmin,
+			max: pmax,
+			value: pval,
+			change: function( event, ui ) {
+				$(this).parents(".price-block").find(".price-range-input").val(f1(ui.value));
+			},
+			slide: function( event, ui ) {
+				$(this).parents(".price-block").find(".price-range-input").val(f1(ui.value));
+			}
+		});
+	});
+
+	$(".price-range-input").blur(function(){
+		if(!isNaN(parseInt($(this).val()))){
+			$(this).parents(".price-block").find(".price-slider").slider( "option", "value", f1(parseInt($(this).val())) );
+		} else{
+			$(this).val($(this).parents(".price-block").find(".price-slider").slider( "option", "value"));
+		}
+	});
+
+	$(".price-range-input").keyup(function(e){
+		if(e.keyCode === 13){
+			$(this).trigger("blur");
+		}
+	});
+
+
+
+
+
+
+	$(".price-slider-log").each(function(){
+		var pmin = parseInt($(this).siblings("input[name=min-val]").val());
+		var pmax = parseInt($(this).siblings("input[name=max-val]").val());
+		var pval = parseInt($(this).parents(".price-block").find(".price-range-input-log").val());
+		$(this).parents(".price-block").find(".price-range-input-log").val(f2(pval));//)
+		$(this).slider({
+			min: pmin,
+			max: pmax,
+			value: pval,
+			change: function( event, ui ) {
+				$(this).parents(".price-block").find(".price-range-input-log").val(f2(parseInt(ui.value)));
+			},
+			slide: function( event, ui ) {
+				$(this).parents(".price-block").find(".price-range-input-log").val(f2(parseInt(ui.value)));
+			}
+		});
+	});
+
+	$(".price-range-input-log").blur(function(){
+		if(!isNaN(parseInt($(this).val()))){
+			$(this).parents(".price-block").find(".price-slider-log").slider( "option", "value", f2_rev(parseInt($(this).val())) );
+		} else{
+			$(this).val($(this).parents(".price-slider-log").find(".price-slider").slider( "option", "value"));
+		}
+	});
+
+	$(".price-range-input-log").keyup(function(e){
+		if(e.keyCode === 13){
+			$(this).trigger("blur");
+		}
+	});
+
+
+
+
+
+
+
+	$(".price-slider-log2").each(function(){
+		var pmin = parseInt($(this).siblings("input[name=min-val]").val());
+		var pmax = parseInt($(this).siblings("input[name=max-val]").val());
+		var pval = parseInt($(this).parents(".price-block").find(".price-range-input-log2").val());
+		$(this).parents(".price-block").find(".price-range-input-log2").val(f3(pval));//)
+		$(this).slider({
+			min: pmin,
+			max: pmax,
+			value: pval,
+			change: function( event, ui ) {
+				$(this).parents(".price-block").find(".price-range-input-log2").val(f3(parseInt(ui.value)));
+			},
+			slide: function( event, ui ) {
+				$(this).parents(".price-block").find(".price-range-input-log2").val(f3(parseInt(ui.value)));
+			}
+		});
+	});
+
+	$(".price-range-input-log2").blur(function(){
+		if(!isNaN(parseInt($(this).val()))){
+			$(this).parents(".price-block").find(".price-slider-log2").slider( "option", "value", f3_rev(parseInt($(this).val())) );
+		} else{
+			$(this).val($(this).parents(".price-slider-log2").find(".price-slider").slider( "option", "value"));
+		}
+	});
+
+	$(".price-range-input-log2").keyup(function(e){
+		if(e.keyCode === 13){
+			$(this).trigger("blur");
+		}
+	});
+
 });
+
+function f1(val){
+	return val===0?1:val;
+}
+
+function f2(val){
+	if(val>=20&&val<=100){
+		return val;
+	} else if(val>100&&val<=120){
+		return 100 + ((val-100)*5);
+	} else if(val>120&&val<=160){
+		return 200 + ((val-120)*20);
+	} else if(val>160&&val<=180){
+		return 1000 + ((val-160)*50);
+	}
+	return val;
+}
+
+function f2_rev(val){
+	if(val>=20&&val<=100){
+		return val;
+	} else if(val>100&&val<=200){
+		return 100 + Math.floor((val-100)/5);
+	} else if(val>200&&val<=1000){
+		return 120 + Math.floor((val-200)/20);
+	} else if(val>1000&&val<=2000){
+		return 160 + Math.floor((val-1000)/50);
+	}
+	return val;
+}
+
+
+function f3(val){
+	if(val == 0){
+		return 1;
+	}else if(val>=1&&val<=50){
+		return val;
+	} else if(val>50&&val<90){
+		return 50 + ((val-50)*5);
+	} else if(val==90){
+		return 254;
+	}
+	return val;
+}
+
+function f3_rev(val){
+	if(val == 0){
+		return 1;
+	} else if(val>=1&&val<=50){
+		return val;
+	} else if(val>50&&val<=254){
+		return 50 + Math.floor((val-50)/5);
+	}
+	return val;
+}
